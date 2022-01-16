dbf3fbffa2f94949a056c3b2fed209485ac1ab98
rudnfcks <rudnfcks@naver.com>
2022-01-16 13:59:02 +0900

    project setting

---
+++ PartyGroup_Java_Server/src/main/java/com/iptime/rudnfcks/PartyGroup/PartyGroupApplication.java
@@ -0,0 +1,13 @@
+package com.iptime.rudnfcks.PartyGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyGroupApplication.class, args);
+	}
+
+}
---
+++ PartyGroup_Java_Server/src/test/java/com/iptime/rudnfcks/PartyGroup/PartyGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iptime.rudnfcks.PartyGroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
