8bac90d60a7d8023ce9d5bae31c5a3988131b57b
rudnfcks <rudnfcks@naver.com>
2022-03-16 22:15:01 +0900

    server Test

--- partyGroup_Back/src/main/java/com/rudnfcks/partyGroup/config/WebConfig.java
+++ partyGroup_Back/src/main/java/com/rudnfcks/partyGroup/config/WebConfig.java
@@ -1,2 +1,15 @@
-package com.rudnfcks.partyGroup.config;public class WebConfig {
+package com.rudnfcks.partyGroup.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","http://partyGroup.iptime.org")
+                .allowedMethods("GET", "PUT", "DELETE", "POST");
+    }
 }
